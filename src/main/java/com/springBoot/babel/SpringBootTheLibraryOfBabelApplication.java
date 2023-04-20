e918a6a17cd94fbfebeef4650c96a7da8c390a8f
RoelsWolf <wolf.roels@student.hogent.be>
2023-04-20 15:17:33 +0200

    first commit

---
+++ src/main/java/com/springBoot/babel/SpringBootTheLibraryOfBabelApplication.java
@@ -0,0 +1,13 @@
+package com.springBoot.babel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTheLibraryOfBabelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTheLibraryOfBabelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springBoot/babel/SpringBootTheLibraryOfBabelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.babel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTheLibraryOfBabelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
